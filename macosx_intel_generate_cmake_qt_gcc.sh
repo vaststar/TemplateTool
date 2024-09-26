d06ed0d900d31cf11fdd2db39369fe59cd9d4c6c
thzhu <thzhu@cisco.com>
2024-09-26 16:43:42 +0800

    add mac cmake

---
+++ macosx_intel_generate_cmake_qt_gcc.sh
@@ -0,0 +1,40 @@
+#!/bin/sh -e
+
+ROOT_DIR="$(pwd)"
+SOURCE_DIR="${ROOT_DIR}"
+BUILD_PATH="${ROOT_DIR}/build"
+OUTPUT_DIR="${ROOT_DIR}/output/build"
+GRAPHVIZ_FILE="${BUILD_PATH}/make_graph.dot"
+# if [ ! -d "$DIRECTORY" ]; then
+#   echo "$DIRECTORY does not exist."
+# fi
+OSX_SYSROOT="$(xcrun --sdk macosx --show-sdk-path)"
+
+CMAKE="${ROOT_DIR}/tools/platforms/macosx/universal/cmake/CMake.app/Contents/bin/cmake"
+
+echo "****************************************************"
+echo "ENABLE_PCH is ${ENABLE_PCH}"
+echo "PCH_ENABLE_PARAMETER is ${PCH_ENABLE_PARAMETER}"
+echo "****************************************************"
+
+${CMAKE} --version
+
+${CMAKE} -S "${SOURCE_DIR}" \
+         -B "${BUILD_PATH}" \
+        #  -G"Xcode" \
+        #  -T buildsystem=12 \
+         --graphviz="${GRAPHVIZ_FILE}" \
+         -DCMAKE_OSX_ARCHITECTURES="x86_64" \
+         -DCLANG_DEBUG_INFORMATION_LEVEL_LINE_TABLES_ONLY=YES \
+         -DCMAKE_OSX_SYSROOT="${OSX_SYSROOT}" \
+         -DCMAKE_RUNTIME_OUTPUT_DIRECTORY="${OUTPUT_DIR}" \
+         -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY="${OUTPUT_DIR}/lib" \
+         -DCMAKE_LIBRARY_OUTPUT_DIRECTORY="${OUTPUT_DIR}/lib" \
+         -DCMAKE_INSTALL_BINDIR="." \
+         -DCMAKE_INSTALL_PREFIX="${BUILD_PATH}/output" \
+          $@
+
+# settings_path="SparkMacDesktop/WebexTeams.xcodeproj/project.xcworkspace/xcuserdata/${USER}.xcuserdatad"
+# mkdir -p "${settings_path}"
+# echo "Create ${settings_path}/WorkspaceSettings.xcsettings"
+# cp WorkspaceSettings_Workaround.xcsettings "${settings_path}/WorkspaceSettings.xcsettings"
