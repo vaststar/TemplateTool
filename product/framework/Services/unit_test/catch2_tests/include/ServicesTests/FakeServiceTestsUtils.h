01a0aefc8f8345b778634c9eba1e295e9f416474
thzhu <thzhu@cisco.com>
2025-07-30 15:58:55 +0800

    move ut

--- product/framework/Services/unit_test/catch2_tests/include/ServicesTests/FakeServiceTestsUtils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <ucf/CoreFramework/ICoreFramework.h>
-#include <memory>
-namespace FakeServiceTestsUtils
-{
-    std::shared_ptr<ucf::framework::ICoreFramework> buildFakeCoreFramework();
-} // namespace FakeServiceTestsUtils
