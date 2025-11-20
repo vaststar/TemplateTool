de9066818dbcbd61543cc317f934e5cf6a2d94cf
thzhu <thzhu@cisco.com>
2025-11-20 16:28:43 +0800

    fix ctor

--- product/framework/Services/ServiceDeclaration/include/ucf/Services/ServiceDeclaration/IService.h
+++ product/framework/Services/ServiceDeclaration/include/ucf/Services/ServiceDeclaration/IService.h
@@ -10,8 +10,14 @@ namespace ucf::service{
 class SERVICE_EXPORT IService
 {
 public:
-    virtual std::string getServiceName() const = 0;
+    IService() = default;
+    IService(const IService&) = delete;
+    IService(IService&&) = delete;
+    IService& operator=(const IService&) = delete;
+    IService& operator=(IService&&) = delete;
     virtual ~IService() = default;
+public:
+    [[nodiscard]] virtual std::string getServiceName() const = 0;
     void initComponent();
 protected:
     virtual void initService() = 0;
