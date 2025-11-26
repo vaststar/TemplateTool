7f602edd6e59043ed20708be4b6070faf681804d
thzhu <thzhu@cisco.com>
2025-11-26 17:11:28 +0800

    move translation into resource

--- product/application/qt/UIAuxiliary/UIResourceLoader/UIResourceTranslation/translations/app_translations_en.ts
+++ product/application/qt/UIAuxiliary/UIResourceLoader/UIResourceTranslation/translations/app_translations_en.ts
@@ -4,7 +4,7 @@
 <context>
     <name>AppSystemTray</name>
     <message>
-        <location filename="../../UIView/AppSystemTray/qml/AppSystemTray.qml" line="15"/>
+        <location filename="../../../../UIView/AppSystemTray/qml/AppSystemTray.qml" line="15"/>
         <source>Quit</source>
         <translation type="unfinished"></translation>
     </message>
@@ -12,7 +12,7 @@
 <context>
     <name>MainWindowContent</name>
     <message>
-        <location filename="../../UIView/MainWindow/qml/MainWindowContent.qml" line="64"/>
+        <location filename="../../../../UIView/MainWindow/qml/MainWindowContent.qml" line="64"/>
         <source>IopenCamera</source>
         <translation type="unfinished"></translation>
     </message>
@@ -20,12 +20,12 @@
 <context>
     <name>MainWindowFootBar</name>
     <message>
-        <location filename="../../UIView/MainWindowFootBar/qml/MainWindowFootBar.qml" line="14"/>
+        <location filename="../../../../UIView/MainWindowFootBar/qml/MainWindowFootBar.qml" line="14"/>
         <source>‹</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UIView/MainWindowFootBar/qml/MainWindowFootBar.qml" line="25"/>
+        <location filename="../../../../UIView/MainWindowFootBar/qml/MainWindowFootBar.qml" line="25"/>
         <source>⋮</source>
         <translation type="unfinished"></translation>
     </message>
@@ -33,66 +33,66 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../../UIView/AppSystemTray/src/AppSystemTrayController.cpp" line="21"/>
-        <location filename="../../UIView/MainWindowSideBar/src/MainWindowSideBarController.cpp" line="20"/>
+        <location filename="../../../../UIView/AppSystemTray/src/AppSystemTrayController.cpp" line="21"/>
+        <location filename="../../../../UIView/MainWindowSideBar/src/MainWindowSideBarController.cpp" line="20"/>
         <source>my test window title bar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UIView/MainWindowFootBar/src/MainWindowFootBarController.cpp" line="20"/>
+        <location filename="../../../../UIView/MainWindow/src/MainWindowController.cpp" line="37"/>
+        <source>my test window aa</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../UIView/MainWindowFootBar/src/MainWindowFootBarController.cpp" line="20"/>
         <source>my footer bar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../UIView/MainWindowTitleBar/src/MainWindowTitleBarController.cpp" line="23"/>
+        <location filename="../../../../UIView/MainWindowTitleBar/src/MainWindowTitleBarController.cpp" line="23"/>
         <source>oo window title bar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build/codegen/UIResourceStringLoader/ResourceStringMap.cpp" line="16"/>
+        <location filename="../../../../../../../build/codegen/UIResourceStringLoader/ResourceStringMap.cpp" line="16"/>
         <source>OK</source>
         <translation>ok</translation>
     </message>
     <message>
-        <location filename="../../../../../build/codegen/UIResourceStringLoader/ResourceStringMap.cpp" line="17"/>
+        <location filename="../../../../../../../build/codegen/UIResourceStringLoader/ResourceStringMap.cpp" line="17"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build/codegen/UIResourceStringLoader/ResourceStringMap.cpp" line="18"/>
+        <location filename="../../../../../../../build/codegen/UIResourceStringLoader/ResourceStringMap.cpp" line="18"/>
         <source>Close</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build/codegen/UIResourceStringLoader/ResourceStringMap.cpp" line="19"/>
+        <location filename="../../../../../../../build/codegen/UIResourceStringLoader/ResourceStringMap.cpp" line="19"/>
         <source>Title</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build/codegen/UIResourceStringLoader/ResourceStringMap.cpp" line="20"/>
-        <source>aabcdef</source>
+        <location filename="../../../../../../../build/codegen/UIResourceStringLoader/ResourceStringMap.cpp" line="20"/>
+        <source>aab</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build/codegen/UIResourceStringLoader/ResourceStringMap.cpp" line="32"/>
+        <location filename="../../../../../../../build/codegen/UIResourceStringLoader/ResourceStringMap.cpp" line="32"/>
         <source>{{0}} of {{1}} completed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../../../build/codegen/UIResourceStringLoader/ResourceStringMap.cpp" line="33"/>
+        <location filename="../../../../../../../build/codegen/UIResourceStringLoader/ResourceStringMap.cpp" line="33"/>
         <source>test param {{0}}, only 1 param</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../UIView/MainWindow/src/MainWindowController.cpp" line="37"/>
-        <source>my test window z</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>UTWindow</name>
     <message>
-        <location filename="../../UIComponent/UTComponent/UTWindow/UTWindow.qml" line="12"/>
+        <location filename="../../../../UIComponent/UTComponent/UTWindow/UTWindow.qml" line="12"/>
         <source>test ut window</source>
         <translation type="unfinished"></translation>
     </message>
