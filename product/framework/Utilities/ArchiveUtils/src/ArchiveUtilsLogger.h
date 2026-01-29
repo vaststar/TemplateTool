c3da11d493fae569118857682cfeea5cad697b7d
thzhu <thzhu@cisco.com>
2026-01-29 17:18:21 +0800

    fix install

--- product/framework/Utilities/ArchiveUtils/src/ArchiveUtilsLogger.h
+++ product/framework/Utilities/ArchiveUtils/src/ArchiveUtilsLogger.h
@@ -1,10 +1,12 @@
 #pragma once
 
-#include <ucf/Utilities/LoggerUtils/LoggerUtils.h>
+#include <ucf/Utilities/LoggerUtils/LoggerExport.h>
 
-#define ARCHIVE_LOG_TRACE(msg)   UCF_LOG_TRACE("ArchiveUtils", msg)
-#define ARCHIVE_LOG_DEBUG(msg)   UCF_LOG_DEBUG("ArchiveUtils", msg)
-#define ARCHIVE_LOG_INFO(msg)    UCF_LOG_INFO("ArchiveUtils", msg)
-#define ARCHIVE_LOG_WARNING(msg) UCF_LOG_WARNING("ArchiveUtils", msg)
-#define ARCHIVE_LOG_ERROR(msg)   UCF_LOG_ERROR("ArchiveUtils", msg)
-#define ARCHIVE_LOG_FATAL(msg)   UCF_LOG_FATAL("ArchiveUtils", msg)
+static inline const char* ArchiveUtilsLoggerName = ucf::utilities::kAppLoggerName;
+static inline const char* ArchiveUtilsLoggerTag = "ArchiveUtils";
+
+#define ARCHIVE_LOG_DEBUG(msg)   UCF_LOG_DEBUG(ArchiveUtilsLoggerTag, msg, ArchiveUtilsLoggerName)
+#define ARCHIVE_LOG_INFO(msg)    UCF_LOG_INFO(ArchiveUtilsLoggerTag, msg, ArchiveUtilsLoggerName)
+#define ARCHIVE_LOG_WARNING(msg) UCF_LOG_WARN(ArchiveUtilsLoggerTag, msg, ArchiveUtilsLoggerName)
+#define ARCHIVE_LOG_ERROR(msg)   UCF_LOG_ERROR(ArchiveUtilsLoggerTag, msg, ArchiveUtilsLoggerName)
+#define ARCHIVE_LOG_FATAL(msg)   UCF_LOG_FATAL(ArchiveUtilsLoggerTag, msg, ArchiveUtilsLoggerName)
