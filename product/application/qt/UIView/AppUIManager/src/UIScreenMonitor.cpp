bc62bdbaea1b38a78bb99a486f6cc1106fda87d5
thzhu <thzhu@cisco.com>
2026-06-30 12:20:34 +0800

    add log

--- product/application/qt/UIView/AppUIManager/src/UIScreenMonitor.cpp
+++ product/application/qt/UIView/AppUIManager/src/UIScreenMonitor.cpp
@@ -1,5 +1,7 @@
 #include "UIScreenMonitor.h"
 
+#include <string>
+
 #include <QGuiApplication>
 #include <QScreen>
 #include <QTimer>
@@ -10,6 +12,28 @@
 
 #include "UIEvents/UIScreenChangedEvent.h"
 #include "UIViewCommon/LoggerDefine/LoggerDefine.h"
+
+namespace
+{
+// One-line description of a screen's current geometry/DPI for logging.
+std::string describeScreen(const QScreen* screen)
+{
+    if (!screen)
+    {
+        return "<null>";
+    }
+    const QRect g = screen->geometry();
+    const QRect a = screen->availableGeometry();
+    const QString s =
+        QStringLiteral("'%1' geo=%2x%3@(%4,%5) avail=%6x%7@(%8,%9) logicalDpi=%10 dpr=%11")
+            .arg(screen->name())
+            .arg(g.width()).arg(g.height()).arg(g.x()).arg(g.y())
+            .arg(a.width()).arg(a.height()).arg(a.x()).arg(a.y())
+            .arg(screen->logicalDotsPerInch(), 0, 'f', 1)
+            .arg(screen->devicePixelRatio(), 0, 'f', 2);
+    return s.toStdString();
+}
+} // namespace
 
 UIScreenMonitor::UIScreenMonitor(AppContext* appContext, QObject* parent)
     : QObject(parent)
@@ -33,19 +57,24 @@ void UIScreenMonitor::start()
     connect(mDebounceTimer, &QTimer::timeout, this, &UIScreenMonitor::notifyScreenChanged);
 
     const auto screens = QGuiApplication::screens();
+    UIVIEW_LOG_DEBUG("initial screen count=" << static_cast<int>(screens.size()));
     for (QScreen* screen : screens)
     {
+        UIVIEW_LOG_DEBUG("  screen " << describeScreen(screen));
         connectScreen(screen);
     }
 
     connect(qApp, &QGuiApplication::screenAdded, this, [this](QScreen* screen) {
+        UIVIEW_LOG_DEBUG("screenAdded: " << describeScreen(screen));
         connectScreen(screen);
         scheduleNotify();
     });
-    connect(qApp, &QGuiApplication::screenRemoved, this, [this](QScreen*) {
+    connect(qApp, &QGuiApplication::screenRemoved, this, [this](QScreen* screen) {
+        UIVIEW_LOG_DEBUG("screenRemoved: " << (screen ? screen->name().toStdString() : std::string("<null>")));
         scheduleNotify();
     });
-    connect(qApp, &QGuiApplication::primaryScreenChanged, this, [this](QScreen*) {
+    connect(qApp, &QGuiApplication::primaryScreenChanged, this, [this](QScreen* screen) {
+        UIVIEW_LOG_DEBUG("primaryScreenChanged: " << describeScreen(screen));
         scheduleNotify();
     });
 }
@@ -65,16 +94,23 @@ void UIScreenMonitor::connectScreen(QScreen* screen)
     {
         return;
     }
-    connect(screen, &QScreen::availableGeometryChanged, this, [this](const QRect&) {
+    connect(screen, &QScreen::availableGeometryChanged, this, [this, screen](const QRect& g) {
+        UIVIEW_LOG_DEBUG("availableGeometryChanged '" << screen->name().toStdString() << "' -> "
+                         << g.width() << "x" << g.height() << "@(" << g.x() << "," << g.y() << ")");
         scheduleNotify();
     });
-    connect(screen, &QScreen::geometryChanged, this, [this](const QRect&) {
+    connect(screen, &QScreen::geometryChanged, this, [this, screen](const QRect& g) {
+        UIVIEW_LOG_DEBUG("geometryChanged '" << screen->name().toStdString() << "' -> "
+                         << g.width() << "x" << g.height() << "@(" << g.x() << "," << g.y() << ")");
         scheduleNotify();
     });
-    connect(screen, &QScreen::logicalDotsPerInchChanged, this, [this](qreal) {
+    connect(screen, &QScreen::logicalDotsPerInchChanged, this, [this, screen](qreal dpi) {
+        UIVIEW_LOG_DEBUG("logicalDotsPerInchChanged '" << screen->name().toStdString() << "' -> "
+                         << dpi << " (dpr=" << screen->devicePixelRatio() << ")");
         scheduleNotify();
     });
-    connect(screen, &QScreen::physicalDotsPerInchChanged, this, [this](qreal) {
+    connect(screen, &QScreen::physicalDotsPerInchChanged, this, [this, screen](qreal dpi) {
+        UIVIEW_LOG_DEBUG("physicalDotsPerInchChanged '" << screen->name().toStdString() << "' -> " << dpi);
         scheduleNotify();
     });
 }
@@ -105,6 +141,14 @@ void UIScreenMonitor::notifyScreenChanged()
     }
 
     UIVIEW_LOG_DEBUG("screen configuration changed, broadcasting UIScreenChangedEvent");
+    const auto screens = QGuiApplication::screens();
+    UIVIEW_LOG_DEBUG("current screen count=" << static_cast<int>(screens.size())
+                     << ", primary=" << describeScreen(QGuiApplication::primaryScreen()));
+    for (QScreen* screen : screens)
+    {
+        UIVIEW_LOG_DEBUG("  screen " << describeScreen(screen));
+    }
+
     UIScreenChangedEvent event;
     bus->send(&event);
 }
